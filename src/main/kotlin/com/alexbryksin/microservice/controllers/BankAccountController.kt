ae1a10c1b1552a465106765dfd76d6e2d125bd31
Alexander <alexander.bryksin@yandex.ru>
2022-10-01 13:45:18 +0300

    feature: add bank account custom repository

--- src/main/kotlin/com/alexbryksin/microservice/controllers/BankAccountController.kt
+++ src/main/kotlin/com/alexbryksin/microservice/controllers/BankAccountController.kt
@@ -4,21 +4,45 @@ import com.alexbryksin.microservice.dto.CreateBankAccountRequest
 import com.alexbryksin.microservice.dto.DepositAmountRequest
 import com.alexbryksin.microservice.dto.SuccessBankAccountResponse
 import com.alexbryksin.microservice.dto.of
+import com.alexbryksin.microservice.repositories.BankAccountPostgresRepositoryImpl
 import com.alexbryksin.microservice.services.BankAccountService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.withTimeout
 import org.slf4j.LoggerFactory
+import org.springframework.data.domain.PageRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.math.BigDecimal
 import java.util.*
 
 
 @RestController
 @RequestMapping(path = ["/api/v1/bank"])
 @Tag(name = "Bank Account", description = "Bank Account controller REST Endpoints")
-class BankAccountController(private val bankAccountService: BankAccountService) {
+class BankAccountController(
+    private val bankAccountService: BankAccountService,
+    private val bankAccountPostgresRepositoryImpl: BankAccountPostgresRepositoryImpl
+) {
+
+    @GetMapping(path = ["all/balance"])
+    suspend fun findAllAccounts(
+        @RequestParam(name = "min", defaultValue = "0") min: BigDecimal,
+        @RequestParam(name = "max") max: BigDecimal,
+        @RequestParam(name = "page", defaultValue = "0") page: Int,
+        @RequestParam(name = "size", defaultValue = "10") size: Int,
+    ) = coroutineScope {
+        try {
+            val pageRequest = PageRequest.of(page, size)
+            val bankAccounts = bankAccountPostgresRepositoryImpl.findByBalanceAmount(min, max, pageRequest)
+            log.info("bankAccounts: ${bankAccounts?.size}")
+            bankAccounts?.map { SuccessBankAccountResponse.of(it) }
+        } catch (ex: Exception) {
+            log.error("find all", ex)
+        }
+    }
 
     @PutMapping(path = ["{id}"])
     @Operation(method = "depositAmount", summary = "deposit amount", operationId = "depositAmount")
--- src/main/kotlin/com/alexbryksin/microservice/repositories/BankAccountPostgresRepositoryImpl.kt
+++ src/main/kotlin/com/alexbryksin/microservice/repositories/BankAccountPostgresRepositoryImpl.kt
@@ -1,10 +1,52 @@
 package com.alexbryksin.microservice.repositories
 
+import com.alexbryksin.microservice.domain.BankAccount
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.reactive.awaitFirst
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
+import org.springframework.data.relational.core.query.Criteria
+import org.springframework.data.relational.core.query.Query
+import org.springframework.r2dbc.core.DatabaseClient
 import org.springframework.stereotype.Repository
+import java.math.BigDecimal
 
 
 @Repository
-class BankAccountPostgresRepositoryImpl(private val r2dbcEntityTemplate: R2dbcEntityTemplate) {
+class BankAccountPostgresRepositoryImpl(private val template: R2dbcEntityTemplate, private val databaseClient: DatabaseClient) {
 
+    suspend fun findByBalanceAmount(min: BigDecimal, max: BigDecimal, pageable: Pageable): PageImpl<BankAccount>? = coroutineScope {
+        val query = Query.query(Criteria.where("balance").between(min, max))
+
+
+        val accountsList = template.select(query.with(pageable), BankAccount::class.java)
+            .asFlow()
+            .buffer(100)
+            .toList()
+
+        val count = template.select(query, BankAccount::class.java).count().awaitFirst()
+
+        PageImpl(accountsList, pageable, count)
+
+//            val bankAccounts = template.databaseClient.sql("select * from microservices.bank_accounts where balance between :min and :max")
+//                .bind("min", min)
+//                .bind("max", max)
+//                .map { row, meta ->
+//                    BankAccount(
+//                        id = row.get("id", UUID::class.java),
+//                        email = row.get("email", String::class.java) ?: "",
+//                        phone = row.get("phone", String::class.java) ?: "",
+//                        balance = row.get("balance", BigDecimal::class.java) ?: BigDecimal.ZERO,
+//                        currency = row.get("currency", Currency::class.java) ?: Currency.USD,
+//                        createdAt = row.get("createdAt", LocalDateTime::class.java) ?: LocalDateTime.now(),
+//                        updatedAt = row.get("updatedAt", LocalDateTime::class.java) ?: LocalDateTime.now(),
+//                    )
+//                }
+//                .flow()
+//                .toList()
+    }
 }
